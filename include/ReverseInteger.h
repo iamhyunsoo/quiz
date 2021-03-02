7c79c18f03de6b836d84d8dd4aa7003c10a225b7
Hyunsoo-Ubuntu <hsleenet@gmail.com>
2021-03-02 15:54:20 -0500

    20210302 15:54

---
+++ include/ReverseInteger.h
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    static int reverse(int x) {
+        long long rev {0};
+        
+        while (x)
+        {
+            rev = (rev * 10) + (x % 10);
+            x = x / 10;
+            
+        }
+        return (rev > INT_MAX || rev < INT_MIN)?0:rev;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,13 @@
-#include "LongestPalindrome.h"
+#include "MyAtoi.h"
 
 #include <algorithm>
 #include <iostream>
 #include <string.h>
-
-
-using namespace std;
-
 #include <iostream>
 #include <vector>
-class Solution1{
-public:
-int myAtoi(string s) 
-    {
-        int ret {0};
-        bool isNeg = false;
-        for (const char&  x : s)
-        { 
-            if (x == 32)
-            {
-              continue;
-            }
-            else if (x == 45)
-            {
-                isNeg = true;
-                continue;
-            }
-            else if(x < 48 && x > 57)
-            {
-                return isNeg ? ret * -1 : ret;
-            }
+using namespace std;
 
-            ret = ret * 10 + (x - 48);
-        }
-
-        return isNeg ? ret * -1 : ret;
-    }
-};
 int main()
 {
-    std::vector<int> a{ 1,2, 3,4,5,6};
-    if (2 == 3)
-    {
-
-    }
-    else
-    {
-        cout << "sibal\n";
-    }
-
-    std::vector<int> b{ 3,4 };
-    Solution1 s;
-    cout << s.myAtoi(std::string("   234 sdf")) << endl;
+    cout << myAtoi("   -341") << endl;
 }
