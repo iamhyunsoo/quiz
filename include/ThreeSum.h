15900a2c4dc5f9ba6800cf69d136febcc9010a51
Kyle Lee <56422173+kylehslee@users.noreply.github.com>
2021-03-07 10:28:59 -0500

    Update ThreeSum.h

--- include/ThreeSum.h
+++ include/ThreeSum.h
@@ -1,51 +1,49 @@
 #include <algorithm>
 #include <vector>
+std::vector< std::vector< int > > ThreeSum(std::vector<int> vec)
+{
+	std::vector< std::vector< int > > ret;
+	int size = vec.size();
+	if (size == 0 || (size == 1 && vec[0] == 0))
+	{
+		return ret;
+	}
+	
+	std::sort(vec.begin(), vec.end());
 
-using namespace std;
-
-class Solution {
-public:
-
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        std::vector<std::vector<int>> ret;
+	int l{ 0 };
+	int r{ 0 };
+	for (int i = 0; i < size; i++)
+	{
+		if (i > 0 && vec[i] == vec[i - 1])
+		{
+			continue;
+		}
         
-        if (nums.size() == 0 || (nums.size() == 1 && nums[0] == 0))
-            return ret;
-        sort(nums.begin(), nums.end());
-        
-        int vecSize = nums.size();
-        int l;
-        int r;
-        for (int i = 0; i < vecSize; i++)
-        {
-            
-            if (i > 0 && nums[i] == nums[i-1])
-                continue;
-            
-            l = i + 1;
-            r = vecSize  - 1;
-            
-            while (l < r)
-            {
-                if ((nums[i] + nums[l] + nums[r]) > 0)
-                {
-                    --r;
-                }
-                else if ((nums[i] + nums[l] + nums[r]) < 0)
-                {
-                    ++l;
-                }
-                else
-                {
-                    ret.emplace_back(std::vector<int>{nums[i], nums[l], nums[r]});
-                    ++l;
-                    while (nums[l] == nums[l - 1] && l < r)
-                    {
-                        ++l;
-                    }
-                }
-            }
-        }
-        return ret;
-    }
-};
+        l = i + 1;
+		r = size - 1;
+
+		while (l < r)
+		{
+			if (vec[i] + vec[l] + vec[r] > 0)
+			{
+				--r;
+			}
+			else if (vec[i] + vec[l] + vec[r] < 0)
+			{
+				++l;
+			}
+			else
+			{
+				ret.emplace_back(std::vector<int>{vec[i], vec[l], vec[r]});
+				++l;
+
+				while (vec[l] == vec[l - 1] && l < r)
+				{
+					++l;
+				}
+			}
+		}
+	}
+	return ret;
+}
