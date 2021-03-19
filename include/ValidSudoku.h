3c27fb8266234b0a9d442b3c93c3d5d8ad5f08e0
Kyle Lee <56422173+kylehslee@users.noreply.github.com>
2021-03-19 10:41:43 -0400

    Create ValidSudoku.h

---
+++ include/ValidSudoku.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution 
+{
+public:
+    static bool isValidSudoku(vector<vector<char> >& board) {
+        int rows[9][10];
+        int cols[9][10];
+        int boxes[9][10];
+
+        for (int i = 0; i < board.size(); i++)
+        {
+            for (int j = 0; j < board[0].size(); j++)
+            {
+                if (board[i][j] != '.')
+                {
+                    int num = board[i][j] - '0';
+                    if (++rows[i][num] > 1 || ++cols[j][num] > 1 || ++boxes[(i / 3) * 3 + j / 3][num] > 1)
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
