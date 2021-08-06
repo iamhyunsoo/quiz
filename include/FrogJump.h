e331693d7c09bb507237f79d471f0db8f3cce2c8
Hyunsoo Lee <56422173+kylehslee@users.noreply.github.com>
2021-08-06 16:21:22 -0400

    20210805 1000

--- include/FrogJump.h
+++ include/FrogJump.h
@@ -1,1 +1,35 @@
-#pragma once
+
+#include <vector>
+bool canCross(std::vector<int>& stones) 
+{
+	int size = stones.size();
+	std::vector<std::vector<bool>> dp(size, std::vector<bool>(size + 1, false));
+
+	dp[0][1] = true;
+	int dist{ 0 };
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			dist = stones[i] - stones[j];
+			if (dist < 0 || dist > size || !dp[j][dist])
+			{
+				continue;
+			}
+			dp[i][dist] = true;
+			if (dist - 1 >= 0)
+			{
+				dp[i][dist - 1] = true;
+			}
+			if (dist + 1 <= size)
+			{
+				dp[i][dist + 1] = true;
+			}
+			if (i == size - 1)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
