3021b230a162fb6ed5c418158d616cab9ff70ee6
Hyunsoo <hsleenet@gmail.com>
2022-01-11 23:43:55 -0500

    Fix errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,6 @@
-#include <map>
-#include <iostream>
-#include <stack>
-#include <string>
-#include <unistd.h>
-#include <vector>
-#include <list>
-#include <unordered_set>
-#include <unordered_map>
-#include <iomanip>
+#include <stdio.h>
 
 int main()
 {
-
-	return 0;
+	printf("hello\n");
 }
-
-    void dec(std::string key) {
-
-        // If the key doesn't exist, just leave.
-        if (!bucketOfKey.count(key))
-            return;
-
-        // Maybe insert the key in previous bucket and update the lookup.
-        auto prev = bucketOfKey[key], bucket = prev--;
-        bucketOfKey.erase(key);
-        if (bucket->value > 1) {
-            if (bucket == buckets.begin() || prev->value < bucket->value - 1)
-                prev = buckets.insert(bucket, {bucket->value - 1, {}});
-            prev->keys.insert(key);
-            bucketOfKey[key] = prev;
-        }
-        
-        // Remove the key from its old bucket.
-        bucket->keys.erase(key);
-        if (bucket->keys.empty())
-            buckets.erase(bucket);
-    }
-
-    std::string getMaxKey() {
-        cout << "Line58::" << buckets.rbegin()->value << endl;
-        return buckets.empty() ? "" : *(buckets.rbegin()->keys.begin());
-    }
-    
-    std::string getMinKey() {
-        return buckets.empty() ? "" : *(buckets.begin()->keys.begin());
-    }
