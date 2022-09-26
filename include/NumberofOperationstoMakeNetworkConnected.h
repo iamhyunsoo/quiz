41ecafd663dee2f8db5550453d06be8f74323e7d
Hyunsoo Lee <56422173+kylehslee@users.noreply.github.com>
2022-09-26 11:13:33 -0400

    Update NumberofOperationstoMakeNetworkConnected.h

--- include/NumberofOperationstoMakeNetworkConnected.h
+++ include/NumberofOperationstoMakeNetworkConnected.h
@@ -1,3 +1,56 @@
+// Two solutions
+
+// Classic union find with ranks
+class Solution 
+{
+public:
+    int getParent(int n, std::vector<int>& parent)
+    {
+        return parent[n] = parent[n] == n ? n : getParent(parent[n], parent);
+    }
+    void merge(int u, int v, std::vector<int>& parent, std::vector<int>& rank)
+    {
+        int a = getParent(u, parent);
+        int b = getParent(v, parent);
+        if (rank[a] == rank[b])
+        {
+            ++rank[a];
+        }
+        
+        if (rank[a] > rank[b])
+        {
+            parent[b] = a;
+        }
+        else
+        {
+            parent[a] = b;
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) { return -1; }
+        std::vector<int> parent(n);
+        std::vector<int> rank(n);
+        
+        std::iota(parent.begin(), parent.end(), 0);
+        
+        for (auto const& con : connections)
+        {
+            merge(con[0], con[1], parent, rank);
+        }
+        
+        int roots = 0;
+        for (size_t i = 0; i < n; ++i)
+        {
+            if (parent[i] == i)
+            {
+                ++roots;
+            }
+        }
+        return roots - 1;
+    }
+};
+
+// dfs with union graph
 class Solution 
 {
 private:
